f8b7170041a919fd824eada86ed15d7f8482cba7
Keagen Talsma <keegenjt@gmail.com>
2022-12-17 14:53:07 -0500

    Implemented toggle

--- dxcamFisher.py
+++ dxcamFisher.py
@@ -3,14 +3,16 @@ import cv2;
 import os.path;
 from pymouse import PyMouse;
 import time;
+import keyboard;
 
 # Use enum for state tracking
 import enum;
 class State(enum.Enum):
-    IDLE = 0; # Just standing (doing nothing)
-    CASTED_OUT = 1; # Fishing pole casted out
-    REELING_IN_FISH = 2; # Holding right click to reel in fish
-    FISHING_IDLE = 3; # Idle when fish is pulling
+    DISABLED = 0; # When autofisher is disabled
+    IDLE = 1; # Just standing (doing nothing)
+    CASTED_OUT = 2; # Fishing pole casted out
+    REELING_IN_FISH = 3; # Holding right click to reel in fish
+    FISHING_IDLE = 4; # Idle when fish is pulling
 
 # Setup images resource path
 res = os.path.join(os.path.dirname(__file__), "res\\");
@@ -44,13 +46,28 @@ screenCam = dxcam.create();
 screenCam.start(target_fps=60);
 
 # Vars for state logic
-state = State.IDLE;
+state = State.DISABLED;
 fishingIdleTime = time.time(); # Used to recast after fish is caught (or failed)
 
 # Add delay for now
 time.sleep(3);
 
 while (True):
+    # Check to enable or disable
+    if (keyboard.is_pressed("g")):
+        if (state == State.DISABLED):
+            state = State.IDLE;
+            time.sleep(2);
+        else:
+            # Need to release mouse if pulling in fish
+            if (state == State.REELING_IN_FISH):
+                mouse.release(int(screenWidth/2), int(screenHeight/2), button = 2);
+            # Reel in if casted out
+            if (state == State.CASTED_OUT):
+                mouse.click(int(screenWidth/2), int(screenHeight/2), button = 2);
+            state = state.DISABLED;
+            time.sleep(2);
+
     # Get latest screen image
     screen = cv2.cvtColor(screenCam.get_latest_frame(), cv2.COLOR_RGB2GRAY);
 
@@ -72,7 +89,7 @@ while (True):
 
         case State.FISHING_IDLE:
             # Check if no longer catching fish
-            if (fishingIdleTime + 5 < time.time()):
+            if (fishingIdleTime + 3.5 < time.time()):
                 state = State.IDLE;
             # Check to start reeling in
             pullResults = cv2.matchTemplate(screen, imgPull, cv2.TM_SQDIFF_NORMED);
@@ -83,7 +100,7 @@ while (True):
         
         case State.REELING_IN_FISH:
             # Check if no longer catching fish
-            if (fishingIdleTime + 5 < time.time()):
+            if (fishingIdleTime + 3.5 < time.time()):
                 state = State.IDLE;
             # Check to stop reeling in
             stopPullResults = cv2.matchTemplate(screen, imgStopPull, cv2.TM_SQDIFF_NORMED);
