83a1749655a9fd1e946d0becbf19fe46df2b65f8
Keagen Talsma <keegenjt@gmail.com>
2023-02-15 19:34:40 -0500

    Simplified for catching non-fish

--- autofish.py
+++ autofish.py
@@ -24,21 +24,13 @@ if (not os.path.exists(res)):
 
 # Create file paths for fishing images
 imgCatch = os.path.join(res, "fishFound.png");
-imgPull = os.path.join(res, "fishPull.png");
-imgStopPull = os.path.join(res, "fishNoPull.png");
 
 # Ensure all neccesary images can be loaded
 if (not os.path.exists(imgCatch)):
     raise Exception("Image file(s) not found");
-if (not os.path.exists(imgPull)):
-    raise Exception("Image file(s) not found");
-if (not os.path.exists(imgStopPull)):
-    raise Exception("Image file(s) not found");
 
 # Load actual images
 imgCatch = cv2.imread(imgCatch, cv2.IMREAD_GRAYSCALE);
-imgPull = cv2.imread(imgPull, cv2.IMREAD_GRAYSCALE);
-imgStopPull = cv2.imread(imgStopPull, cv2.IMREAD_GRAYSCALE);
 
 # Setup for mouse input
 mouse = PyMouse();
@@ -59,12 +51,7 @@ while (True):
             state = State.IDLE;
             time.sleep(2);
         else:
-            # Need to release mouse if pulling in fish
-            if (state == State.REELING_IN_FISH):
-                mouse.release(int(screenWidth/2), int(screenHeight/2), button = 2);
-            # Reel in if casted out
-            if (state == State.CASTED_OUT):
-                mouse.click(int(screenWidth/2), int(screenHeight/2), button = 2);
+            keyboard.press_and_release('s');
             state = state.DISABLED;
             time.sleep(2);
 
@@ -84,27 +71,7 @@ while (True):
             if (cv2.minMaxLoc(catchResults)[0] < 0.01):
                 # If found, reel in
                 mouse.click(int(screenWidth/2), int(screenHeight/2), button=2);
-                fishingIdleTime = time.time(); 
-                state = State.FISHING_IDLE;
+                keyboard.press_and_release('s');
+                state = State.IDLE;
+                time.sleep(0.5);
 
-        case State.FISHING_IDLE:
-            # Check if no longer catching fish
-            if (fishingIdleTime + 3.5 < time.time()):
-                state = State.IDLE;
-            # Check to start reeling in
-            pullResults = cv2.matchTemplate(screen, imgPull, cv2.TM_SQDIFF_NORMED);
-            if (cv2.minMaxLoc(pullResults)[0] < 0.05):
-                mouse.press(int(screenWidth/2), int(screenHeight/2), button=2);
-                fishingIdleTime = time.time(); 
-                state = State.REELING_IN_FISH;
-        
-        case State.REELING_IN_FISH:
-            # Check if no longer catching fish
-            if (fishingIdleTime + 3.5 < time.time()):
-                state = State.IDLE;
-            # Check to stop reeling in
-            stopPullResults = cv2.matchTemplate(screen, imgStopPull, cv2.TM_SQDIFF_NORMED);
-            if (cv2.minMaxLoc(stopPullResults)[0] < 0.09):
-                mouse.release(int(screenWidth/2), int(screenHeight/2), button=2);
-                fishingIdleTime = time.time(); 
-                state = State.FISHING_IDLE;
